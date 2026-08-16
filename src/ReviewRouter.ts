522e1859d2053c5c54be951c27fb214182065810
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-08-16 16:29:44 +0530

    Initial commit

---
+++ src/ReviewRouter.ts
@@ -0,0 +1,55 @@
+import Router from "express";
+import prisma from "../src/Modules/prisma";
+import "dotenv/config"
+
+const ReviewRouter = Router();
+
+ReviewRouter.get("/", async (req, res) => {
+    if (!req.body || !req.body.password) {
+        const reviews = await prisma.review.findMany({
+            where: {
+                isPublic: true
+            }
+        });
+
+        return res.json(reviews);
+    }
+
+    const { password } = req.body;
+
+    if (password !== process.env.ADMIN_PASSWORD) {
+        return res.status(401).json({ error: "Unauthorized access" });
+    }
+
+    const reviews = await prisma.review.findMany();
+    return res.json(reviews);
+});
+
+ReviewRouter.post("/create", async (req, res) => {
+    if (!req.body) {
+        return res.status(400).json({ error: "Body cannot be null" });
+    }
+
+    const { rating, isPublic, body } = req.body;
+    const invalid = !rating || isPublic == null || !body || typeof rating !== "number" || typeof isPublic !== "boolean" || typeof body !== "string";
+
+    if (invalid) {
+        return res.status(400).json({ error: "Invalid request body" });
+    }
+
+    if (rating < 1 || rating > 5) {
+        return res.status(400).json({ error: "Rating must be between 1 and 5" });
+    }
+
+    await prisma.review.create({
+        data: {
+            rating,
+            isPublic,
+            body
+        }
+    });
+
+    return res.status(201).json({ message: `Review of rating ${rating} successfully created` });
+});
+
+export default ReviewRouter;
