604ac0007b38767275f01b900dd235cded8bc909
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-08-17 21:15:31 +0530

    Successfully added an admin endpoint to reply to a review

--- src/Routers/AdminRouter.ts
+++ src/Routers/AdminRouter.ts
@@ -16,6 +16,43 @@ AdminRouter.get("/", async (req, res) => {
     return res.json(reviews);
 });
 
+AdminRouter.post("/reply/:id", async (req, res) => {
+    if (!req.body) {
+        return res.status(400).json({ error: "Body cannot be null" });
+    }
+
+    const id = Number(req.params.id);
+    const { reply } = req.body;
+
+    if (!reply) {
+        return res.status(400).json({ error: "Reply cannot be null" });
+    }
+
+    const foundReview = await prisma.review.findUnique({
+        where: {
+            id: id
+        }
+    });
+
+    if (!foundReview) {
+        return res.status(404).json({ error: `Review of id ${id} does not exist` });
+    }
+
+    await prisma.review.update({
+        where: {
+            id: id
+        },
+
+        data: {
+            admins: {
+                push: reply
+            }
+        }
+    });
+
+    return res.json({ message: `Successfully created new reply to review of id ${id} with the text '${reply}'` });
+});
+
 AdminRouter.get("/analyze/:id", async (req, res) => {
 
     const id = Number(req.params.id);
