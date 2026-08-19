f5804f11f2f382ea8005ac673f15ba5726e2d667
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-08-19 18:00:54 +0530

    Created a /Components folder and a basic Reviews.tsx component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,30 +1,11 @@
-import Swal from "sweetalert2";
+import Reviews from "./Components/Reviews";
 
 function App() {
-  function handleClick() {
-    Swal.fire({
-      title: "SweetAlert2",
-      text: "This is an alert using the SweetAlert2 library!",
-      icon: "success",
-      confirmButtonText: "Nice!"
-    });    
-  }
-
-  async function getName() {
-    await Swal.fire({
-      title: "Question",
-      text: "What's your name? ",
-      input: "text",
-      inputPlaceholder: "Name here"
-    });
-  }
-
   return (
     <>
-      <button onClick={handleClick}>Click me!</button>
-      <button onClick={getName}>Answer question</button>
+      <Reviews/>
     </>
-  )
+  );
 }
 
 export default App;
