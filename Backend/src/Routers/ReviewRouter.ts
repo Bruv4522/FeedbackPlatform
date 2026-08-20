6b59d437e78849545d891cd99579d60f51e0c0ab
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-08-20 15:28:51 +0530

    Successfully created /report/:id endpoint in ReviewRouter.ts and filled it with working code

--- Backend/src/Routers/ReviewRouter.ts
+++ Backend/src/Routers/ReviewRouter.ts
@@ -45,4 +45,41 @@ ReviewRouter.post("/create", async (req, res) => {
     return res.status(201).json({ message: `Review of rating ${rating} successfully created` });
 });
 
+ReviewRouter.post("/report/:id", async (req, res) => {
+    if (!req.body) {
+        return res.status(400).json({ error: "Body cannot be null" });
+    }
+
+    const id = Number(req.params.id);
+    const { report } = req.body;
+
+    if (!report) {
+        return res.status(400).json({ error: "Report cannot be null" });
+    }
+
+    const foundReview = await prisma.review.findUnique({
+        where: {
+            id: id
+        }
+    });
+
+    if (!foundReview) {
+        return res.status(404).json({ error: `Review of id ${id} does not exist` });
+    }
+
+    await prisma.review.update({
+        where: {
+            id: id
+        },
+
+        data: {
+            reports: {
+                push: report
+            }
+        }
+    });
+
+    return res.json({ message: `Successfully reported review of id ${id}, we will get back to your shortly` });
+});
+
 export default ReviewRouter;
