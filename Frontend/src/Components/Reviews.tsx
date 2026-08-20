9daa6bce5d6637e23ef5e81d3751e34be02ebe8d
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-08-20 16:27:13 +0530

    Created a weird problem in Reviews.tsx which is going to be fixed later, will now start working on a rate limiter for the backend

--- Frontend/src/Components/Reviews.tsx
+++ Frontend/src/Components/Reviews.tsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from "react";
 
+type Review = {
+    id: number,
+    dateTime: string,
+    rating: number,
+    isPublic: boolean,
+    body: string,
+    admins: string[],
+    reports: string[]
+};
+
 function Reviews() {
-    const [reviews, setReviews] = useState(null);
+    const [reviews, setReviews] = useState<Review[]>([]);
 
     useEffect(() => {
         fetch("http://localhost:3000")
@@ -9,8 +19,19 @@ function Reviews() {
             .then(data => setReviews(data));
     }, []);
 
+    if (reviews.length == 0) {
+        return <p>No reviews found</p>;
+    }
+
+    setReviews([...reviews]);
+
     return (
         <>
+            {reviews.map((review) => {
+                <div key={review.id}>
+                    <li key={review.id}>{review.body}</li>
+                </div>
+            })}
         </>
     );
 }
